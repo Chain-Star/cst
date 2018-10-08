1c3f31c02478b0cbf674782d6a5ce501053fc77f
MS-20180424ERSP\Administrator <227690008@163.com>
2018-10-08 10:29:32 +0800

    项目展示身份认证判断

--- application/index/controller/Project.php
+++ application/index/controller/Project.php
@@ -26,11 +26,47 @@ class Project extends HomeBase
     }
     public function state_check($project_id = 0)
     {
-       
+       if (!Session::has('user_id'))
+        {
+            $this->error(Lang::get("remind11"));
+        }
+        if (!Session::has('identity') || Session::get('identity') != 1)
+        {
+            $this->error(Lang::get("p_audit_1"));
+        }
+        if ($project_id != 0)
+        {
+            if (!$this->project_model->where(['create_user_id' => $this->user_id, 'id' => $project_id])->count())
+            {
+                $this->error(Lang::get("p_audit"));
+            }
+        }
     }
     public function index()
     {
-        
+        $where = [];
+        $page  = 1;
+        // if (!empty($data['seacheamil']))
+        // {
+        //     $where[] = ['email', 'like', '%' . $data['seacheamil'] . '%'];
+        // }
+        // if (!empty($data['seachname']))
+        // {
+        //     $where[] = ['name', 'like', '%' . $data['seachname'] . '%'];
+        // }
+        // if (!empty($data['seachnum']))
+        // {
+        //     $where[] = ['item_number', 'like', '%' . $data['seachnum'] . '%'];
+        // }
+        $limit        = 12;
+        $count        = $this->project_model->where($where)->count();
+        $project_list = $this->project_model->where($where)
+            ->limit(($page - 1) * $limit, $limit)->order('id', 'DESC')->select();
+        $this->assign('page', $page);
+        $this->assign('limit', $limit);
+        $this->assign('count', $count);
+        $this->assign('project_list', $project_list);
+        return $this->fetch('project_index');
     }
 
     public function add($step = 0, $project_id = 0)
